d64977cbdee7b834f16cd4cfa9ee52b9d0fcabbc
robified <daorob@gmail.com>
2019-07-14 22:41:21 -0500

    Created game logic with objects (JavaScript).

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,33 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 };
 
@@ -15,24 +36,13 @@ function flipcard(cardId) {
 	if (cardsInPlay.length === 2) {
 		
 	}
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 
 
 flipcard(0);
-flipcard(1);
+flipcard(2);
 checkForMatch();
-
-
-
-
-
-//	var cardOne = cards[0];
-//	cardsInPlay.push(cardOne);
-//	console.log("User flipped " + cardOne);
-
-
-//	var cardTwo = cards[2];
-//	cardsInPlay.push(cardTwo);
-//	console.log("User flipped " + cardTwo);
